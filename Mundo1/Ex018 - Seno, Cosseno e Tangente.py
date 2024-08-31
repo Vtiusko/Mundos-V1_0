6f9163fd95186f226ca94e6e281b20d2eb0f8178
Victor <victorgsg7272@gmail.com>
2024-08-31 15:41:42 -0300

    Adicionando script conforme feito no smartphone

--- Mundo1/Ex018 - Seno, Cosseno e Tangente.py
+++ Mundo1/Ex018 - Seno, Cosseno e Tangente.py
@@ -9,7 +9,9 @@ from emoji import emojize
 
 
 while True:
-    print(f'{" CONVERSOR: SENO, COSSENO E TANGENTE ":=^45}\n{"(RADIANO)":-^45}\n')
+    texto1 = ' CONVERSOR: SENO, COSSENO E TANGENTE '
+    texto2 = ' (\033[32mRADIANO\033[m) '
+    print(f'{texto1:=^45}\n{texto2:-^53}\n')
 
     sleep(0.6)
 
@@ -27,8 +29,20 @@ while True:
     opcoes = int(opcoes)
 
     if opcoes == 1:
-        print(f'O SENO de \033[32m{angulo}\033[m é igual à: \033[32m{sin(radians(angulo)):.2f}\033[m ou \033[32m{ceil(sin(radians(angulo)))}\033[m')
+        print(f'\n\033[32m>>>\033[m O SENO de \033[32m{angulo}\033[m é igual à: \033[32m{sin(radians(angulo)):.2f}\033[m ou \033[32m{ceil(sin(radians(angulo)))}\033[m')
     elif opcoes == 2:
-        print(f'O COSSENO de \033[32m{angulo}\033[m é igual à: \033[32m{cos(radians(angulo)):.2f}\033[m ou \033[32m{ceil(cos(radians(angulo)))}\033[m')
+        print(f'\n\033[32m>>>\033[m O COSSENO de \033[32m{angulo}\033[m é igual à: \033[32m{cos(radians(angulo)):.2f}\033[m ou \033[32m{ceil(cos(radians(angulo)))}\033[m')
     elif opcoes == 3:
-        print(f'A TANGENTE de \033[32m{angulo}\033[m é igual à: \033[32m{tan(radians(angulo)):.2f}\033[m ou \033[32m{ceil(tan(radians(angulo)))}\033[m')
+        print(f'\n\033[32m>>>\033[m A TANGENTE de \033[32m{angulo}\033[m é igual à: \033[32m{tan(radians(angulo)):.2f}\033[m ou \033[32m{ceil(tan(radians(angulo)))}\033[m')
+
+    pergunta = input('\nDeseja calcular novamente [ S / N ]: ').upper()
+    while pergunta not in 'SN' or pergunta == '':
+        print('Você deve digitar "S" ou "N"!')
+
+        pergunta = input('\nDeseja calcular novamente [ \033[mS\033[32m / \033[mN\033[32m ]: ').upper()
+
+    if pergunta == 'N':
+        print('\nAté mais!')
+        break
+    else:
+        print('\n')
